0ff85c3d96bf05ebd14ea4f338cb0200e9c8d77b
SamanEN <SamanEslami78@gmail.com>
2025-01-30 18:11:18 +0330

    Add rate app

--- bitpin_blog/bitpin_blog/settings.py
+++ bitpin_blog/bitpin_blog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users.apps.UsersConfig',
-    'blog_posts.apps.BlogPostsConfig'
+    'blog_posts.apps.BlogPostsConfig',
+    'rate.apps.RateConfig'
 ]
 
 MIDDLEWARE = [
