a6181698cfeba5ff54de35b547ffe355b99e1e7f
SamanEN <SamanEslami78@gmail.com>
2025-01-30 16:04:57 +0330

    Add blog posts list view

--- bitpin_blog/blog_posts/urls.py
+++ bitpin_blog/blog_posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogPostCreateView
+from .views import BlogPostCreateView, BlogPostsListView
 
 urlpatterns = [
-    path("create/", BlogPostCreateView.as_view())
+    path("create/", BlogPostCreateView.as_view(), name='create_blog_post'),
+    path("", BlogPostsListView.as_view(), name='blog_posts_list')
 ]
--- bitpin_blog/blog_posts/views.py
+++ bitpin_blog/blog_posts/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import BlogPost
 from .serializers import BlogPostSerializer
 
 class BlogPostCreateView(APIView):
@@ -25,3 +26,12 @@ class BlogPostCreateView(APIView):
         """Will respond with the blog post form page."""
 
         return render(request, 'blog_post_create.html')
+    
+class BlogPostsListView(APIView):
+    """This view will simply return the list of all blogs created by users."""
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        blog_posts = BlogPost.objects.all()
+        return render(request, 'blogs_list.html', {'blog_posts': blog_posts})
