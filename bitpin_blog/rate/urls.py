5b2a870b7feb88c589ad17979c9bc0dd6bc79cb4
SamanEN <SamanEslami78@gmail.com>
2025-01-30 21:23:43 +0330

    Add blog rating stats view

--- bitpin_blog/rate/urls.py
+++ bitpin_blog/rate/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RateCreateView
+from .views import RateCreateView, BlogRatingStatsView
 
 urlpatterns = [
-    path("", RateCreateView.as_view(), name='rate_blog')
+    path("", RateCreateView.as_view(), name='rate_blog'),
+    path("get/<int:blog_id>/", BlogRatingStatsView.as_view(), name='blog_rating_stats')
 ]
--- bitpin_blog/rate/views.py
+++ bitpin_blog/rate/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
+from django.db.models import Avg, Count
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from blog_posts.models import BlogPost
+
 from .serializers import RateSerializer
+from .models import Rate
 
 class RateCreateView(APIView):
     """This view will only handle creating ratings for blogs."""
@@ -20,3 +24,24 @@ class RateCreateView(APIView):
             return redirect(f'/blog/{blog_id}')
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class BlogRatingStatsView(APIView):
+    """This view will fetch displayed info of a blog post's ratings."""
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request, blog_id):
+        """
+        Returns the average rating and the total number of ratings for a given blog post.
+        """
+        blog = get_object_or_404(BlogPost, id=blog_id)
+        
+        rating_data = Rate.objects.filter(blog=blog).aggregate(
+            average_rating=Avg('rating'),
+            total_ratings=Count('id')
+        )
+
+        return Response({
+            "average_rating": rating_data["average_rating"] or 0,
+            "total_ratings": rating_data["total_ratings"]
+        }, status=status.HTTP_200_OK)
